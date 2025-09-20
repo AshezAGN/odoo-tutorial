70a21398dd2b57d90bcca41daf70244e7d4372f9
Akash Nair <92288769+AshezAGN@users.noreply.github.com>
2025-09-20 16:16:26 +0400

    added new fields

--- estate/models/die_master.py
+++ estate/models/die_master.py
@@ -41,8 +41,23 @@ class DieMaster(models.Model):
         required=true,
         # computed = ""
     )
-    cylinder_teeth = fields.
-    
+    cylinder_teeth = fields.Many2One(
+        domain='[]',
+        # comodel_name='cylinder_master',
+        # relation='cylidner_master.cylinder_teeth'
+    )
+    cylinder_repeat = fields.Float(
+        string="Cylinder Repeat (mm)"
+        computed="_compute_cylinder_repeat"
+    )
+    around_ups = fields.Integer()
+    around_gaps = fields.Float()
+    total_ups = fields.Integer()
+    magnetic_cylinder = fields.Integer()
+    die_option = fields.Many2One()
+    face_stock = fields.Many2One()
+    liner = fields.Many2One()
+    # die_status = fields.Selection()
 
     @api.model
     def create(self, vals):
@@ -51,22 +66,40 @@ class DieMaster(models.Model):
             vals['name'] = self.env['ir.sequence'].next_by_code('diecutter.seq') or 'New'
         return super(DieCutter, self).create(vals)
 
-    # @api.onchange("repeat_length")
-    # def _onchange_get_recommended_teeth(self):
-    #     # When repeat_length is inputted, suggest 6 best cylinder teeth that best work for it.
-    #     for record in self:
-    #         if not rec.repeat_length:
-    #             return {
-    #                 'domain': {'selected_teeth_id': [('id', '=', False)]}
-    #             }
-    #         else:
-    #             all_teeth = self.env['cylinder.teeth'].search([])
-    #             for teeth in all_teeth:
-    #                 teeth.around_ups = math.floor(teeth.cylinder_teeth / (record.repeat_length * 2.5))
-    #                 teeth.around_gaps = (cylinder_repeat / teeth.around_ups) + record.repeat_length
-    #                 sorted_teeths = sorted(all_teeth, key=lambda t: t.around_gaps)
-                
-                
+    # Updates each time repeat_length changes
+    @api.onchange("repeat_length")
+    def _onchange_get_recommended_teeth(self):
+        # When repeat_length is inputted, suggest 6 best cylinder teeth that best work for it.
+        for record in self:
+            if not rec.repeat_length:
+                return {
+                    'domain': {'cylinder_teeth': []}
+                }
+            else:
+                all_teeth = self.env['cylinder.teeth'].search([])
+                # Calculate around ups and around gaps.
+                for teeth in all_teeth:
+                    teeth.around_ups = math.floor(teeth.cylinder_teeth / (record.repeat_length * 2.5))
+                    teeth.around_gaps = (cylinder_repeat / teeth.around_ups) + record.repeat_length
+                sorted_teeths = sorted(all_teeth, key=lambda t: t.around_gaps)
+                # Get 6 unique smallest values.
+                seen = set()
+                unique_teeths = []
+                for teeth in sorted_teeth:
+                    if teeth.aroud_gaps not in seen:
+                        unique_teeths.append(teeth.cylinder_teeth)
+                    if len(unique_teeths) >= 6:
+                        break
+                return {
+                    # 'id' ?
+                    'domain': {'cylinder_teeth': [('id','in',unique_teeths)]}
+                }
+
+    @api.depends('cylinder_teeth')
+    def _compute_cylinder_repeat(self):
+        for record in self:
+            if record.cylinder_teeth:
+                record.cylinder_repeat = 3.125 * record.cylinder_teeth
 
 
 class CylinderTeeth(models.Model):
@@ -84,4 +117,13 @@ class DieType(models.Model):
     die_cutter = fields.One2Many(
         comodel_name="die_master",
         inverse_name="die_type"
+    )
+
+class DieOptions(models.Model):
+    _name = "die_options"
+
+    name = fields.Char(required = true)
+    die_cutter = fields.One2Many(
+        comodel_name="die_master",
+        inverse_name="die_option"
     )
